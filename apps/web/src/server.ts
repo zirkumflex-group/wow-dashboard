d19ce37cb6857cbea76f3a06c4db0af6619cd86f
Jan-GJ <janjuettermann@gmail.com>
2026-03-26 00:45:37 +0100

    fix: build

--- apps/web/src/server.ts
+++ apps/web/src/server.ts
@@ -3,6 +3,8 @@ import handler, { createServerEntry } from "@tanstack/react-start/server-entry";
 
 globalThis.Response = FastResponse;
 
+//sdsd
+
 export default createServerEntry({
     fetch(request) {
         return handler.fetch(request);
