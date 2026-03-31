26a10423b97833062a3dd3ff45ce3bfd53dc1e61
yungtristxn <oetris@gmail.com>
2026-03-31 15:56:07 +0200

    Revert "Inject public runtime env for Convex URLs"
    This reverts commit 57cb07da82d46edd5d2cea7a324153305297593a.

--- apps/web/src/lib/auth-server.ts
+++ apps/web/src/lib/auth-server.ts
@@ -1,10 +1,8 @@
 import { convexBetterAuthReactStart } from "@convex-dev/better-auth/react-start";
-import { getPublicRuntimeEnv } from "@/lib/runtime-env";
-
-const runtimeEnv = getPublicRuntimeEnv();
+import { env } from "@wow-dashboard/env/web";
 
 export const { handler, getToken, fetchAuthQuery, fetchAuthMutation, fetchAuthAction } =
   convexBetterAuthReactStart({
-    convexUrl: runtimeEnv.VITE_CONVEX_URL,
-    convexSiteUrl: runtimeEnv.VITE_CONVEX_SITE_URL,
+    convexUrl: env.VITE_CONVEX_URL,
+    convexSiteUrl: env.VITE_CONVEX_SITE_URL,
   });
